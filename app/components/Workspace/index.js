891d5b7d8eabc094d846dad53502d708bc081581
mr.meo <tien@zodinet.com>
2018-08-12 23:31:40 +0700

    update code

--- app/components/Workspace/index.js
+++ app/components/Workspace/index.js
@@ -1,13 +1,28 @@
 import React, { Component } from 'react';
+import {fabric} from 'fabric'
+
 
 class Workspace extends Component {
-  render() {
-    return (
-      <div className="repoDetailInner">
-        Workspace
-      </div>
-    );
-  }
+	constructor() {
+		super();
+	}
+
+	componentDidMount() {
+		let canvas = new fabric.Canvas('workspace', {
+			backgroundColor: 'red',
+			selectionColor: 'blue',
+			selectionLineWidth: 2
+		});           
+	}
+
+
+	render() {
+		return (
+			<div className="repoDetailInner">
+				<canvas id="workspace"></canvas>
+			</div>
+		);
+	}
 }
 
 export default Workspace;
--- app/components/WorkspaceToolbar/index.js
+++ app/components/WorkspaceToolbar/index.js
@@ -1,11 +1,16 @@
 import React, { Component } from 'react';
+import { Icon, Button } from 'antd';
+import styles from './style.css';
 
 class WorkspaceToolbar extends Component {
   render() {
     return (
-      <div>
-
-      </div>
+		<div className={styles.workspaceToolbar}>
+			<ul className={styles.toolbarIconList}>
+				<li><Button shape="circle" icon="upload" /></li>
+				<li><Button shape="circle" icon="plus" /></li>
+			</ul>
+		</div>
     );
   }
 }
