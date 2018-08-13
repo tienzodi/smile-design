956ed7b014bdd8548dc5131e9ff87c6c48d5045e
mr.meo <tien@zodinet.com>
2018-08-13 07:31:13 +0700

    update code

--- app/components/Workspace/index.js
+++ app/components/Workspace/index.js
@@ -1,25 +1,73 @@
 import React, { Component } from 'react';
-import {fabric} from 'fabric'
+import {fabric} from 'fabric';
 
+import dataTemplate from "../../../resources/templates/template-1/config.json";
+
+import styles from './style.css';
+
+const sampleImage = require('../../../resources/images/human.png');
 
 class Workspace extends Component {
 	constructor() {
 		super();
+
+		this.canvas = null;
 	}
+
+	saveRef = (ref) => this.containerNode = ref
 
 	componentDidMount() {
-		let canvas = new fabric.Canvas('workspace', {
-			backgroundColor: 'red',
-			selectionColor: 'blue',
-			selectionLineWidth: 2
-		});           
+		let canvas = new fabric.Canvas('workspace', { selection: true });      
+		canvas.setHeight(1000);
+		canvas.setWidth(800);
+
+		this.canvas = canvas;
+
+		this.renderPatientImage();
+		this.renderTeethTemplate();
 	}
 
+	renderPatientImage() {
+		var canvas = this.canvas;
+
+		fabric.Image.fromURL(sampleImage, function(img) {
+			img.selectable = false;
+			img.scaleToWidth(800);
+			canvas.add(img);
+		});
+
+		canvas.renderAll();      
+	}
+
+	renderTeethTemplate() {
+		const templatePath = '../resources/templates/template-1';
+		let data = dataTemplate.data;
+		let canvas = this.canvas;
+
+		data.forEach(element => {
+			fabric.Image.fromURL(`${templatePath}/${element.name}`, function(img) {
+				img.set({
+				  left: element.left,
+				  top: element.top,
+				  angle: element.angle
+				});
+
+				img.scaleToWidth(element.width);
+		  
+				img.perPixelTargetFind = true;
+				img.targetFindTolerance = 4;
+				img.hasControls = img.hasBorders = true;
+		  
+				canvas.add(img);
+			});
+		});
+
+	}
 
 	render() {
 		return (
-			<div className="repoDetailInner">
-				<canvas id="workspace"></canvas>
+			<div ref={this.saveRef} className="repoDetailInner">
+				<canvas className={styles.workspace} id="workspace"></canvas>
 			</div>
 		);
 	}
